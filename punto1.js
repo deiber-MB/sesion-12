6509e03151fe645f8ec48e83efe10f64d453ba98
deiber-MB <deiber.monterrosa.ttc.939@unilibre.edu.co>
2024-11-14 20:19:48 -0500

    punto 2

--- punto1.js
+++ punto1.js
@@ -1,8 +1,20 @@
----let numeros = [5,1,8,6,7,8,6,9];
+let numeros = [5,1,8,6,7,8,6,9];
 let sumatoria=0;
-
-for (i=0; i<numeros.length; i++){
-    sumatoria=+numeros[i];    
+let promedio=0;
+ 
+alert("llegue a mi js");
+ 
+for (let i=0; i<numeros.length; i++){
+    console.log(numeros[i]);
+    sumatoria+=numeros[i];    
 }
-
-document.getElementById("resultado").value = sumatoria;
+ 
+console.log(sumatoria);
+promedio=sumatoria/numeros.length;
+ 
+document.write("Este es el resultado de la sumatoria " , sumatoria,"<br>");
+document.write("Este es el resultado de la promedio " , promedio,"<br>");
+ 
+document.getElementById("resultado").value("jjjj");
+ 
+//document.getElementById("promedio").value = promedio;
